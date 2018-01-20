48f6804dddd6df4c7e1a857c09562311251d0289
Rebecca Tong <rtong@buffalo.edu>
2018-01-20 21:45:51 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,7 +22,7 @@ class Student
 
   def add_student_attributes(attributes_hash)
     attributes_hash.each do|key,value|
-    self.send("#{key}=",value)
+    self.send("#{key=}",value)
   end
     self
   end
