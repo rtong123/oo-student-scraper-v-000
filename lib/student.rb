a396d49d097b499029bad07ea08f0286f3e6d8a4
Rebecca Tong <rtong@buffalo.edu>
2018-01-20 22:10:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 
 class Student
 
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url, :facebook
 
   @@all = []
 
